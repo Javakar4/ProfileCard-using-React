fde13f2661eb21060e930932c223c4377abf6215
Javakar4 <javakarjava44@gmail.com>
2024-05-29 23:36:50 +0530

    Files Uploaded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const usersData = [
+  {
+      name:"Chuki",
+      city:"Chennai",
+      description:"Front-End Developer",
+      skills:["Front-End Development","HTML","CSS","JS","React.Js","Node.Js"],
+      online:true,
+      profile:"Images/UserProfile-1.jpg"
+  },
+  {
+      name:"Bheem",
+      city:"Erode",
+      description:"Back-End Developer",
+      skills:["Back-End Development","HTML","CSS","Node.Js","MongoDB","Java","Python"],
+      online:true,
+      profile:"Images/UserProfile-2.jpg"
+  },
+  {
+      name:"Raju",
+      city:"Coimbatore",
+      description:"Full-Stack Developer",
+      skills:["Full-Stack Development","HTML","CSS","JS","React.Js","Node.Js","MongoDB","Python"],
+      online:false,
+      profile:"Images/UserProfile-3.jpg"
+  },
+]
+
+function User(props){
+  return(
+    <div className='card-container'>
+      <span className={props.online?"pro online":"pro offline"}>{props.online?"ONLINE":"OFFLINE"}</span>
+      <img className="img" src={props.profile} alt="User1" />
+      <div className="line"></div>
+      <h3>{props.name}</h3>
+      <h3>{props.city}</h3>
+      <p>{props.discription}</p>
+      <div className="buttons">
+        <button className="primary">Message</button>
+        <button className="primary outline">Following</button>
+      </div>
+      <div className="skills">
+        <h6>Skills</h6>
+         <ul>
+          {props.skills.map((skill,index)=>(
+              <li key={index}>{skill}</li>
+          ))}
+        </ul> 
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+const App = () => {
+
+return (
+    <>
+    {
+      usersData.map((user,index)=>(
+          <User key={index}
+          name={user.name}
+          city={user.city}
+          description={user.description}
+          skills={user.skills}
+          online={user.online}
+          profile={user.profile}
+          />
+      ))
+    }
+    </>
+)
+}
+
+
+
+export default App
